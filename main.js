ccda5c3ccc6063d854e5b4ea52964388d9492918
DudinDmitriy <dood_pvs@mail.ru>
2022-10-25 19:02:18 +0300

    321

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
+import {createApp} from 'vue'  
 
-import {createApp} from '../Vue' ; 
-
-import App from './components/App.vue'; 
+import App from './components/App.vue' 
 
 createApp(App).mount('#app') ;
+
