aedf1e3a68705f6b953829974fef550dc657b2b6
DudinDmitriy <dood_pvs@mail.ru>
2022-11-08 15:02:53 +0300

    123

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,35 @@
   <vHeader
    nameUser = 'Usr1'
    numberGoods = 'sdfsdf'
-   :priceOrder = "priceOrder"
+   :priceOrder = priceOrder
   />
   <p>-----------------------</p>
   <vCatalog/>  
+  
+  <input 
+  type = "number"
+  v-model="priceOrder" />
+  
+  <button
+    v-on:click = "checkType">
+    check type
+  </button>
+  <div>{{priceOrder}}</div>
+  <div>
+    <table class = "ahead">
+      <th class="ahead_category">
+        Category 
+      </th>
+      <th class="ahead_user">
+        User   
+      </th>
+      <th class = "ahead_card">
+        Card
+      </th>
+    </table>
+  </div>
+
+
 </template>
 
 <script>
@@ -15,18 +40,36 @@ import vCatalog from './components/v-catalog.vue'
 
 export default {
 
-components:{
+  components:{
     vHeader,
     vCatalog
-},
-data(){
+  },
+  data(){
   return {
     priceOrder:100
+    }
+  },
+  methods:{
+    checkType(){
+      console.log(typeof(this.priceOrder))
+    }
   }
-}
 }
 </script>
 
 <style>
+  .ahead{
+    border: 2px solid black;
+    width: 100%;
+  }
+  .ahead.ahead_category{
+    width: 66%;
+  }
 
+  .ahead.ahead_user{
+    width: 66%;
+  }
+  .ahead_card{
+    width: 66%;
+  }
 </style>
