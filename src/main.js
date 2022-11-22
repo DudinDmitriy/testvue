27722d1fdcd8c8948122b99dbff497c780e51150
DudinDmitriy <dood_pvs@mail.ru>
2022-11-22 20:36:37 +0300

    333

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './components/store'
+import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
+const app = new Vue({
   render: h => h(App), 
   store
-}).$mount('#app')
+})
+app.use(store)
+app.$mount('#app')
